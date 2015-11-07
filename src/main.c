a9d70d7bb5d0f56a9faf53b37f78c37388c28bb6
yaochie <yaochonl@andrew.cmu.edu>
2015-11-06 21:17:28 -0500

    First commit

---
+++ src/main.c
@@ -0,0 +1,180 @@
+#include <pebble.h>
+
+#define COORDS_LAT 0
+#define COORDS_LONG 1
+
+static Window *s_main_window;
+static SimpleMenuLayer *s_main_menu_layer;
+static TextLayer *s_selected_text;
+
+static SimpleMenuSection s_menu_list;
+static SimpleMenuItem items[2];
+
+static Window *s_popup_window;
+static TextLayer *s_popup_text;
+static TextLayer *s_latitude;
+static TextLayer *s_longitude;
+
+static AppSync s_sync;
+static uint8_t s_sync_buffer[32];
+
+enum popupTypes {
+    RANDOM,
+    SELECTED,
+};
+
+static void random_load(Window *window) {
+    Layer* window_layer = window_get_root_layer(window);
+    
+    s_latitude = text_layer_create(GRect(0, 0, 144, 50));
+    s_longitude = text_layer_create(GRect(0, 50, 144, 50));
+    text_layer_set_background_color(s_latitude, GColorClear);
+    text_layer_set_background_color(s_longitude, GColorClear);
+    
+    layer_add_child(window_layer, text_layer_get_layer(s_latitude));
+    layer_add_child(window_layer, text_layer_get_layer(s_longitude));
+}
+
+static void random_unload(Window *window) {
+    text_layer_destroy(s_latitude);
+    text_layer_destroy(s_longitude);
+}
+
+static void selected_load(Window *window) {
+    Layer* window_layer = window_get_root_layer(window);
+    
+    s_popup_text = text_layer_create(layer_get_bounds(window_layer));
+    text_layer_set_background_color(s_popup_text, GColorClear);
+    
+    text_layer_set_text(s_popup_text, "User choice has been selected!");
+    
+    layer_add_child(window_layer, text_layer_get_layer(s_popup_text));
+}
+
+static void selected_unload(Window *window) {
+    text_layer_destroy(s_popup_text);
+}
+
+static void init_popup_window(enum popupTypes type) {
+    s_popup_window = window_create();
+    if (type == RANDOM) {
+        window_set_window_handlers(s_popup_window, (WindowHandlers) {
+            .load = random_load,
+            .unload = random_unload,
+        });
+    } else if (type == SELECTED) {
+        window_set_window_handlers(s_popup_window, (WindowHandlers) {
+            .load = selected_load,
+            .unload = selected_unload,
+        });
+    }
+    
+    window_stack_push(s_popup_window, true);
+}
+
+static void menu_select(int index, void *context) {
+    //print title below!
+    if ((uint32_t)index <= s_menu_list.num_items) {
+        text_layer_set_text(s_selected_text, s_menu_list.items[index].title);
+    }
+    
+    //open new window
+    if (index == 0) {
+        init_popup_window(RANDOM);
+    } else {
+        init_popup_window(SELECTED);
+    }
+}
+
+static void init_menu() {
+    s_menu_list.title = NULL;
+    s_menu_list.num_items = 2;
+    items[0].title = "Random Place";
+    items[0].callback = &menu_select;
+    items[1].title = "Choose a Place";
+    items[1].callback = &menu_select;
+    s_menu_list.items = items;
+}
+
+static void main_window_load(Window *window) {
+    //load main menu
+    Layer* window_layer = window_get_root_layer(window);
+    s_selected_text = text_layer_create(GRect(0, 90, 144, 40));
+    text_layer_set_text(s_selected_text, "Selection goes here");
+    
+    init_menu();
+    s_main_menu_layer = simple_menu_layer_create(GRect(0, 0, 144, 168), window, &s_menu_list, 2, NULL);
+    if (s_main_menu_layer == NULL) {
+        APP_LOG(APP_LOG_LEVEL_ERROR, "Could not create simple menu!");
+    } else {
+        layer_add_child(window_layer, simple_menu_layer_get_layer(s_main_menu_layer));
+        layer_add_child(window_layer, text_layer_get_layer(s_selected_text));
+    }
+}
+
+static void main_window_unload(Window *window) {
+    //destroy main menu
+    simple_menu_layer_destroy(s_main_menu_layer);
+    text_layer_destroy(s_selected_text);
+}
+
+static void inbox_received_callback(DictionaryIterator *iter, void *context) {
+    Tuple* lat = dict_find(iter, COORDS_LAT);
+    Tuple* lon = dict_find(iter, COORDS_LONG);
+    
+    if (lat && lon) {
+        if (s_latitude && s_longitude) {
+            text_layer_set_text(s_latitude, lat->value->cstring);
+            text_layer_set_text(s_longitude, lon->value->cstring);
+        }
+    }
+}
+
+static void sync_changed_handler(const uint32_t key, const Tuple *new_tuple, const Tuple *old_tuple, void *context) {
+    static char s_lat_buffer[32];
+    static char s_lon_buffer[32];
+    if (key == COORDS_LAT) {
+        snprintf(s_lat_buffer, sizeof(s_lat_buffer), "Lat: %s", new_tuple->value->cstring);
+        if (s_latitude) text_layer_set_text(s_latitude, s_lat_buffer);
+    } else {
+        snprintf(s_lon_buffer, sizeof(s_lon_buffer), "Long: %s", new_tuple->value->cstring);
+        if (s_longitude) text_layer_set_text(s_longitude, s_lon_buffer);
+    }
+}
+
+static void sync_error_handler(DictionaryResult dict_error, AppMessageResult app_message_error, void *context) {
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Sync error!");
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Dict code: %d", (int)dict_error);
+    APP_LOG(APP_LOG_LEVEL_ERROR, "AppMessage code: %d", (int)app_message_error);
+}
+
+static void init() {
+    //app_message_register_inbox_received(inbox_received_callback);
+    
+    s_main_window = window_create();
+    window_set_window_handlers(s_main_window, (WindowHandlers) {
+        .load = main_window_load,
+        .unload = main_window_unload,
+    });
+    
+    app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+    
+    Tuplet initial_values[] = {
+        TupletCString(COORDS_LAT, "0"),
+        TupletCString(COORDS_LONG, "0"),
+    };
+    
+    window_stack_push(s_main_window, true);
+    app_sync_init(&s_sync, s_sync_buffer, sizeof(s_sync_buffer), initial_values, ARRAY_LENGTH(initial_values), sync_changed_handler, sync_error_handler, NULL);
+}
+
+static void deinit() {
+    window_destroy(s_main_window);
+    app_sync_deinit(&s_sync);
+}
+
+int main(void) {
+    init();
+    app_event_loop();
+    deinit();
+}
---
+++ src/panoramio.js
@@ -0,0 +1,62 @@
+var range;
+var coordinates;
+
+function setRange(newRange) {
+    range = newRange;
+}
+
+function fetchPanoLocation() {    
+    var req = new XMLHttpRequest();
+    var minx = coordinates.longitude - range;
+    var maxx = coordinates.longitude + range;
+    var miny = coordinates.latitude - range;
+    var maxy = coordinates.latitude + range;
+    var numPhotos = 100;
+    req.open('GET', "http://www.panoramio.com/map/get_panoramas.php?set=public&from=0" +
+             "&to=" + numPhotos + "&minx=" + minx + "&miny=" + miny + "&maxx=" + maxx +
+             "&maxy=" + maxy + "&size=medium");
+    req.onload = function(e) {
+        if (req.readyState == req.DONE) {
+            if (req.status == 200) {
+                console.log("received: " + req.responseText);
+                
+                var response = JSON.parse(req.responseText);
+                var chosenPhoto = response.photos[Math.floor(Math.random() * numPhotos)];
+                console.log(response.count);
+                console.log("lat: " + chosenPhoto.latitude + " long: " + chosenPhoto.longitude);
+                console.log(String(chosenPhoto.latitude));
+                Pebble.sendAppMessage({
+                    0: String(chosenPhoto.latitude),
+                    '1': String(chosenPhoto.longitude)
+                });
+            } else {
+                console.log("Error with request: " + req.status);
+            }
+        }
+    };
+    req.send(null);
+}
+
+function locationSuccess(pos) {
+    coordinates = pos.coords;
+    fetchPanoLocation();
+}
+
+function locationError(err) {
+    console.warn("Location error (" + err.code + "): " + err.message);
+    //send blank result?
+}
+
+var locationOptions = {'timeout': 10000, 'maximumAge': 60000};
+
+Pebble.addEventListener("ready", function(e) {
+    console.log('Javascript app ready and running!');
+    setRange(0.5);
+    navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+});
+
+Pebble.addEventListener("appmessage", function(e) {
+    //handle app message
+    console.log("Got message: " + JSON.stringify(e));
+    navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+});
